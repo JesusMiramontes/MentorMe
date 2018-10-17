ab3a0f5271b0e47187b1662d73a0d4de2c28dfe7
Jesus M <jesus_miramontes@me.com>
2018-10-17 14:28:58 -0500

    Init commit Gem integration Some MVC items

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  get 'welcome/index'
+
+  get 'welcome/Index'
+
+  resources :topics
+  resources :careers
+  resources :professors
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'welcome#index'
+end
