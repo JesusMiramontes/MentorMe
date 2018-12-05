7cba394a365f16aba03330525d5e8959f2ae0fdd
Jesus M <jesus_miramontes@me.com>
2018-12-05 12:31:13 -0600

    Show more info

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,7 @@
 class ReviewsController < ApplicationController
   before_action :set_review, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:show, :index]
-  before_action :authenticate_admin!, only: [:destroy]
+  before_action :authenticate_admin!, except: [:show, :create]
   # GET /reviews
   # GET /reviews.json
   def index
